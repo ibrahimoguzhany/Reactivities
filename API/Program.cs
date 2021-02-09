8dcfc3a156c0d3ef888ad36a1e4daa20afccf4b4
İbrahim Oğuzhan YILMAZ <ibrahimoguzhany@gmail.com>
2021-02-10 02:02:35 +0300

    mediatR added

--- API/Program.cs
+++ API/Program.cs
@@ -25,8 +25,9 @@ namespace API
                 {
                     var context = services.GetRequiredService<DataContext>();
                     context.Database.Migrate();
+                    Seed.SeedData(context);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     var logger = services.GetRequiredService<ILogger<Program>>();
                     logger.LogError(ex, "An error occured during migration");
@@ -34,7 +35,7 @@ namespace API
             }
 
             host.Run();
-        
+
 
         }
 
